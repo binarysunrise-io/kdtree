19fcfc20e53f2c6369b85bd6624e49da5e71bf36
Michael Litchard <michael@schmong.org>
2016-07-09 10:16:48 -0700

    added some method signatures

--- Data/Trees/KdTree/Regions/Internal.hs
+++ Data/Trees/KdTree/Regions/Internal.hs
@@ -28,15 +28,30 @@ import Data.BoundingBox
 --
 
 
-class ( BoundingBox box
+class ( BoundingBox bbox
       , Vector vect
       , Vector center , vect ~ center
-      ) => KdTreeRegional box vect center payload where
+      ) => KdTreeRegional bbox vect center where
   data Axes vect :: *
-  data KdTree vect box payload :: * -> *
-  data Region box center :: * -> *
+  data KdTree vect bbox payload :: * -> *
+  data Region bbox center :: * -> *
 
-instance KdTreeRegional BBox3 Vector3 Vector3 Int where 
+  -- | fromList builds KdTree given list of Vector/payload pairs
+  fromList          :: [(vect,payload)] -> KdTree vect bbox payload center
+  -- | toList generates list of Vector/payload pairs, given a KdTree
+  toList            :: KdTree vect bbox payload center -> [(vect,payload)]
+  -- | nearestNeighbor returns the nearest neighbor of bbox in tree.
+  nearestNeighbor   :: KdTree vect bbox payload center -> bbox -> Maybe bbox
+  -- | nearNeighbors kdtree r bbox returns all neighbors within distance r from bbox in tree.
+  nearNeighbors     :: KdTree vect bbox payload center -> Scalar -> bbox -> [bbox]
+  -- | kNearestNeighbors tree k bbox returns the k closest points to bbox within tree.
+  kNearestNeighbors :: KdTree vect bbox payload center -> Int -> bbox -> [bbox]
+  -- | remove t (v,p) removes (v,p) from t
+  remove :: KdTree vect bbox payload center -> (vect,payload) -> KdTree vect bbox payload center
+  -- | nextAxis gives next Axis in rotation
+  nextAxis :: Axes vect -> Axes vect
+
+instance KdTreeRegional BBox3 Vector3 Vector3 where 
   data Axes Vector3 = X AxisX | Y AxisY | Z AxisZ deriving Show
   data KdTree Vector3 BBox3 Int Vector3
     = KdNode {
